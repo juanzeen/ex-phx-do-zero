c875a23f71fb5ff677e89b782225bbdb482a6de6
juanzeen <jziignerdev@gmail.com>
2024-07-27 17:31:23 -0300

    added hash to the password and the create user function only needing response views

---
+++ banana_bank/lib/banana_bank/users/create.ex
@@ -0,0 +1,11 @@
+defmodule BananaBank.Users.Create do
+  alias BananaBank.Users.User
+  alias BananaBank.Repo
+
+  def call(params) do
+    params
+    |> User.changeset()
+    |> Repo.insert()
+  end
+
+  end
---
+++ banana_bank/priv/repo/migrations/20240727182559_add_users_table.exs
@@ -0,0 +1,15 @@
+defmodule BananaBank.Repo.Migrations.AddUsersTable do
+  use Ecto.Migration
+
+  def change do
+    create table("users") do
+      add :name, :string, null: false
+      add :password_hash, :string, null: false
+      add :email, :string, null: false
+      add :cep, :string, null: false
+
+      timestamps()
+      #adiciona as colunas inserted_at e updated_at
+    end
+  end
+end
